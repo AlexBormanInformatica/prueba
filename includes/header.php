1201749d8b0370db577932237fe2292659cdda6d
Alex <101583846+AlexBormanInformatica@users.noreply.github.com>
2024-01-31 09:11:22 +0100

    avanzando el index

--- includes/header.php
+++ includes/header.php
@@ -3,15 +3,65 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- <link rel="shortcut icon" type="image/x-icon" href="imagenes/<?= htmlspecialchars($_SESSION['nombreWeb']) ?>.ico"> -->
+
     <title>Tienda online de telas, tejidos y accesorios textiles</title>
 
-
+    <!--Fuentes-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Epilogue&family=Quicksand&display=swap" rel="stylesheet">
+
+    <!--Bootstrap 5.2-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <!--Estilos propios-->
     <link rel="stylesheet" href="css/estilos.css">
 
 </head>
 
 <body>
+    <header class="site-header">
+        <div class="div-envio-gratis">
+            <div class="flex">
+                <p class="no-margin"> <img draggable="false" role="img" class="emoji" alt="🚚" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f69a.svg">
+                    España península plazo de entrega de 3 a 5 días | <img draggable="false" role="img" class="emoji" alt="📢" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f4e2.svg"><img draggable="false" role="img" class="emoji" alt="📢" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f4e2.svg">
+                    Envío GRATUITO a partir de 50€ <img draggable="false" role="img" class="emoji" alt="🧵" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f9f5.svg"> </p>
+            </div>
+        </div>
+        <div class="main-header">
+            <div class="primary-header row">
+                <div class="grid-center-col-layout">
+                    <div class="site-header-primary-section-left flex">
+                        <aside class="header-widget-area widget-area site-header-focus-item header-widget-area-inner" data-section="sidebar-widgets-header-widget-1" aria-label="Header Widget 1">
+                            <form class="" role="search" action="" method="get">
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text" id="basic-addon1"><svg class="icono-buscar" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 51.539 51.361" xml:space="preserve">
+                                            <path d="M51.539,49.356L37.247,35.065c3.273-3.74,5.272-8.623,5.272-13.983c0-11.742-9.518-21.26-21.26-21.26 S0,9.339,0,21.082s9.518,21.26,21.26,21.26c5.361,0,10.244-1.999,13.983-5.272l14.292,14.292L51.539,49.356z M2.835,21.082 c0-10.176,8.249-18.425,18.425-18.425s18.425,8.249,18.425,18.425S31.436,39.507,21.26,39.507S2.835,31.258,2.835,21.082z"></path>
+                                        </svg></span>
+                                    <input type="text" class="form-control input-buscar" placeholder="Buscar productos" aria-label="Buscar productos" aria-describedby="basic-addon1">
+                                </div>
+                            </form>
+                        </aside>
+                        <div class="flex">
+                        </div>
+                    </div>
+                    <div class="site-header-primary-section-center site-header-section ast-flex ast-grid-section-center">
+                        <div class="ast-builder-layout-element ast-flex site-header-focus-item" data-section="title_tagline">
+                            <div class="site-branding ast-site-identity" itemtype="https://schema.org/Organization" itemscope="itemscope">
+                                <span>
+                                    <a href="<?php echo BASE_URL; ?>" rel="home">
+                                        <img width="168" height="119" alt="Logo Tienda de telas y fornituras" decoding="async" data-srcset="" data-src="" data-sizes="(max-width: 168px) 100vw, 168px" class="custom-logo lazyloaded" src="imagenes/cropped-LOGO-TELAS-Y-FORNITURAS-COLOR-1-3-168x119.png" sizes="(max-width: 168px) 100vw, 168px" srcset="">
+                                    </a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="">
+
+                    </div>
+                </div>
+            </div>
+    </header>
--- index.php
+++ index.php
@@ -1,12 +1,103 @@
 <?php
 require_once 'includes/config.php';
 require_once 'includes/header.php';
-
 ?>
-<h1>Hola</h1>
-<h2>Subtítulo</h2>
-<h3>Otro subtítulo</h3>
 
-</body>
+<div class="">
+    <section class="top-section">
+        <div class="background-overlay"></div>
+        <div class="column-gap-default">
+            <div class="col-100 top-column">
+                <div class="mb-20">
+                    <h1 class="heading-title">¡Bienvenido a la tienda online de telas y fornituras textiles!</h1>
+                </div>
+                <section class="inner-section">
+                    <div class="section-boxed">
+                        <div class="inner-column col">
+                            <div class="mb-20">
+                                <a href="/productos/tejidos/">
+                                    <img alt="Telas y tejidos" decoding="async" data-srcset="" data-src="" src="imagenes/categorias/telas-y-tejidos.png" srcset="">
+                                </a>
+                            </div>
+                            <div class="mb-20">
+                                <h2 class="title-col elementor-size-default">Telas y tejidos</h2>
+                            </div>
+                        </div>
+                        <div class="inner-column col">
+                            <div class="mb-20">
+                                <a href="/productos/tejidos/">
+                                    <img alt="Fornituras textiles" decoding="async" data-srcset="" data-src="" src="imagenes/categorias/fornituras.png" srcset="">
+                                </a>
+                            </div>
+                            <div class="mb-20">
+                                <h2 class="title-col elementor-size-default">Fornituras textiles</h2>
+                            </div>
+                        </div>
+                        <div class="inner-column col">
+                            <div class="mb-20">
+                                <a href="/productos/tejidos/">
+                                    <img alt="Embalaje textil" decoding="async" data-srcset="" data-src="" src="imagenes/categorias/embalaje.png" srcset="">
+                                </a>
+                            </div>
+                            <div class="mb-20">
+                                <h2 class="title-col elementor-size-default">Embalaje textil</h2>
+                            </div>
+                        </div>
+                        <div class="inner-column col">
+                            <div class="mb-20">
+                                <a href="/productos/tejidos/">
+                                    <img alt="Etiquetaje" decoding="async" data-srcset="" data-src="" src="imagenes/categorias/etiquetas.png" srcset="">
+                                </a>
+                            </div>
+                            <div class="mb-20">
+                                <h2 class="title-col elementor-size-default">Etiquetaje</h2>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section class="inner-section">
+                    <div class="section-boxed">
+                    <div class="inner-column col">
+                            <div class="mb-20">
+                                <a href="/productos/tejidos/">
+                                    <img alt="Herramientas textiles" decoding="async" data-srcset="" data-src="" src="imagenes/categorias/utillaje.png" srcset="">
+                                </a>
+                            </div>
+                            <div class="mb-20">
+                                <h2 class="title-col elementor-size-default">Herramientas textiles</h2>
+                            </div>
+                        </div>
+                        <div class="inner-column col">
+                            <div class="mb-20">
+                                <a href="/productos/tejidos/">
+                                    <img alt="Marcaje textil" decoding="async" data-srcset="" data-src="" src="imagenes/categorias/marcaje.png" srcset="">
+                                </a>
+                            </div>
+                            <div class="mb-20">
+                                <h2 class="title-col elementor-size-default">Marcaje textil</h2>
+                            </div>
+                        </div>
+                        <div class="inner-column col">
+                            <div class="mb-20">
+                                <a href="/productos/tejidos/">
+                                    <img alt="Maniquíes" decoding="async" data-srcset="" data-src="" src="imagenes/categorias/maniquies.png" srcset="">
+                                </a>
+                            </div>
+                            <div class="mb-20">
+                                <h2 class="title-col elementor-size-default">Maniquíes</h2>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </section>
+</div>
 
-</html>
+<!-- Pie de página -->
+<footer class="bg-light text-center p-3">
+    <!-- Contenido del pie de página -->
+</footer>
+<?php
+require_once 'includes/footer.php';
+?>
